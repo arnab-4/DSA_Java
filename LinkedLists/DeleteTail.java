2c7f7ec1a907253e10ee02f08577fc7c7526be6f
Shadmaan1234 <110898977+Shadmaan1234@users.noreply.github.com>
2024-07-30 22:20:26 +0530

    Add files via upload
    updated

---
+++ LinkedLists/DeleteTail.java
@@ -0,0 +1,53 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class DeleteTail {
+    public static Node deleteTail(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+
+    public static void printLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(9);
+        head.next = new Node(5);
+        head.next.next = new Node(15);
+
+        System.out.println("Printing original Linked list:");
+        printLL(head);
+
+        head = deleteTail(head);
+
+        System.out.println("Printing Linked list after deleting tail:");
+        printLL(head);
+    }
+}
